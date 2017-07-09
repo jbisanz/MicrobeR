7b493fe0e9455d7fbd1491af8c4bc1e9174bee4c
Jordan Bisanz <jbisanz@otp1247797ots.ucsf.edu>
2017-07-09 15:14:37 -0700

    Fix for CZM function

--- R/Make.CLR.R
+++ R/Make.CLR.R
@@ -5,7 +5,7 @@
 #' For information on CZM replacement see : Martin-Fernandez, J.A., Hron, K., Templ, M., Filzmoser, P., Palarea-Albaladejo, J. Bayesian-multiplicative treatment of count zeros in compositional data sets. Statistical Modelling 2015; 15 (2): 134-158.
 #' @param OTUTABLE Table of feature/OTU/SV counts where Samples are columns, and IDs are row names
 #' @param PRIOR A numeric value to add before log transformation (default 0.5). *Ignored if CZM=TRUE
-#' @param CZM Should count zero multiplicative method be used instead of adding prior (TRUE/FALSE, defaults to TRUE)
+#' @param CZM Should count zero multiplicative method be used instead of adding simple prior (TRUE/FALSE, defaults to FALSE)
 #' @return Table of CLR-normalized Abundances (log2)
 #' @export
 
@@ -17,7 +17,7 @@ Make.CLR<-function(TABLE, PRIOR, CZM){
   if(missing(PRIOR)){PRIOR=0.5}
   
   if(CZM==TRUE){
-    TABLE <- t(cmultRepl(t(TABLE),  label=0, method="CZM"))
+    TABLE <- t(cmultRepl(t(TABLE),  label=0, method="CZM", output="counts"))
   } else {
     TABLE<-TABLE + PRIOR
   }
