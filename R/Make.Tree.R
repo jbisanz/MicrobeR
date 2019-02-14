58a205f1c3476cef000fe7c406ea2deb1b628cff
Jordan Bisanz <jordan.bisanz@gmail.com>
2019-02-14 09:47:58 -0800

    Updated tree building approaches

--- R/Make.Tree.R
+++ R/Make.Tree.R
@@ -2,41 +2,15 @@
 #'
 #' Generate a phylogenetic tree and optional plot.
 #'
-#' @description Takes a series of sequences and uses muscle and make_phylogeny (Make.Tree.QIIME) OR DECIPHER and phangorn (Make.Tree.R) as per https://f1000research.com/articles/5-1492/v2. The R-method will take considerably longer and becomes unusable for >a few hundred features. For QIIME method: can be used with MacQIIME or QIIME. Will default to MacQIIME locations. A file called Feature_Seqs.tree will be created in your working directory for future use.  Uses midpoint rooting, and fasttree. NOTE: This is using system commands... probabily will only work on mac and linux. If you are using windows reconsider your life choices and consider dual booting Ubuntu.
+#' @description Takes a series of sequences and uses DECIPHER and FastTree (Make.Tree.Fast) OR DECIPHER and phangorn (Make.Tree.R) as per https://f1000research.com/articles/5-1492/v2.
 #' @param FEATURENAMES The names of your features/OTUs/SVs. Will be the row names of your input table in most cases.
 #' @param FEATURESEQS The corresponding sequences for each feature. If this is a dada2 table, then feature names are also feature seqs.
-#' @param MUSCLE (optional) The location of muscle, defaults to /macqiime/bin/muscle
-#' @param MAKEPHY (optional) Location of qiime make_phylogeny.py, defatuls to /macqiime/anaconda/bin/make_phylogeny.py
-#' @param PRINTTREE (optional) Would you like a tree to be printed TRUE/FALSE? defaults to TRUE
+#' @param FASTTREE (optional) The location of the fast tree binary on your system
 #'
 #' @return Returns tree object and prints a phylogenetic tree
 #' @export
 
-Make.Tree.QIIME<-function(FEATURENAMES,FEATURESEQS, MUSCLE, MAKEPHY, PRINTTREE){
-
-  if(missing(MUSCLE)){MUSCLE="/macqiime/bin/muscle"}
-  if(missing(MAKEPHY)){MAKEPHY="/macqiime/anaconda/bin/make_phylogeny.py"}
-  if(missing(PRINTTREE)){PRINTTREE=F}
-
-  fasta<-DNAStringSet(FEATURESEQS)
-  names(fasta)<-FEATURENAMES
-  writeXStringSet(fasta, "Feature_Seqs.fasta")
-
-  system(paste(MUSCLE,"-in Feature_Seqs.fasta -out Feature_Seqs.mfa"))
-  system(paste("source /macqiime/configs/bash_profile.txt;",MAKEPHY,"--tree_method fasttree --root_method=midpoint -i Feature_Seqs.mfa -o Feature_Seqs.tree"))
-
-  TREE<-ape::read.tree("Feature_Seqs.tree")
-
-if (PRINTTREE==TRUE){
-  plot.phylo(TREE, cex=0.3, direction="downwards")
-}
-
-  return(TREE)
-
-}
-
-
-Make.Tree.R<-function(FEATURENAMES,FEATURESEQS, MUSCLE, MAKEPHY, PRINTTREE){
+Make.Tree.R<-function(FEATURENAMES,FEATURESEQS){
 
   fasta<-DNAStringSet(FEATURESEQS)
   names(fasta)<-FEATURENAMES
@@ -61,4 +35,19 @@ Make.Tree.R<-function(FEATURENAMES,FEATURESEQS, MUSCLE, MAKEPHY, PRINTTREE){
 
 }
 
+Make.Tree.Fast<-function(FEATURENAMES,FEATURESEQS, FASTTREE){
+  
+  if(missing(FASTTREE)){FASTTREE="FastTree"}
+  
+  fasta<-DNAStringSet(FEATURESEQS)
+  names(fasta)<-FEATURENAMES
+  
+  alignment <- AlignSeqs(fasta, anchor=NA)
+  randomname<-paste(sample(LETTERS, 20), collapse="")
+  writeXStringSet(alignment, paste0("/tmp/",randomname,".mfa"))
+  system(paste0(FASTTREE, " -nt ", paste0("/tmp/",randomname,".mfa"), " > ", paste0("/tmp/",randomname,".tree")))
+  TREE<-read.tree(paste0("/tmp/",randomname,".tree"))
+  TREE<-midpoint(TREE) #### ADD MIDPOINT TO EXPORTS
+  return(TREE)
+}
 
